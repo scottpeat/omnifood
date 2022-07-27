8b28f4dbfc89b19a0bf6c2c419e0114bb2314988
Scott Peat <scottpeatweb@gmail.com>
2022-07-27 21:47:58 +0100

    Finished sticky navigation

--- js/script.js
+++ js/script.js
@@ -60,6 +60,32 @@ allLinks.forEach(function (link) {
   });
 });
 
+/////////////////////////////////////////////////////
+// Sticky Navigation
+/////////////////////////////////////////////////////
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // IN the Viewpoint
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
 /*
