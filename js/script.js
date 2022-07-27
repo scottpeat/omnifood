06e4bec135c67932e6bf777ca7d1747400d2d4c5
Scott Peat <scottpeatweb@gmail.com>
2022-07-27 20:37:43 +0100

    Working on smooth scrolling in JS

--- js/script.js
+++ js/script.js
@@ -30,6 +30,29 @@ const headerEl = document.querySelector('.header');
 btnNavEl.addEventListener('click', function() {
   headerEl.classList.toggle('nav-open');
 });
+
+///////////////////////////////////////////////
+// SMOOTH SCROLLING ANIMATION
+//////////////////////////////////////////////
+
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach(function(link) {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    if (href !== "#" && href.startsWith('#'))
+    document.querySelector
+  });
+});
+
+
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
 /*
