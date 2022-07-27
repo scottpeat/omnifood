67cf59702ebb0793f14f61b191c1d9250f76d6aa
Scott Peat <scottpeatweb@gmail.com>
2022-07-27 08:16:09 +0100

    Fixed opening and closing mobile navigation and now implementing smooth scrolling

--- js/script.js
+++ js/script.js
@@ -18,11 +18,18 @@ function checkFlexGap() {
 }
 checkFlexGap();
 
+// Set current year
 const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
+// Make mobile navigation work
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
 
+btnNavEl.addEventListener('click', function() {
+  headerEl.classList.toggle('nav-open');
+});
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
 /*
